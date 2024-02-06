05513bbfe86b8b328ca30f0b54a33e3dc380d977
Chris <rikoukuroi@yahoo.com>
2024-02-06 12:05:08 -0500

    Attempt to fix broken images.

--- ContactPro/Services/DataGenService.cs
+++ ContactPro/Services/DataGenService.cs
@@ -47,11 +47,11 @@ namespace ContactPro.Services
                     //✔️ 1. Change images to something not provided by coder foundry.  
                     //2. Make more contacts, set some contacts to categories.
                     //3. See about how to test the email properly...
-                new Contact() {AppUserId = User.Id,FirstName = "Joan", LastName = "Smith", Address1 = "1234 Apple Street", BirthDate = new DateTime(1994, 08, 16), City = "Paris", State = Enums.States.TX, Email = "test@test.com", ZipCode = 21000, PhoneNumber = "0000000000", ImageData = File.ReadAllBytes(Path.Combine(currentDirectory, "wwwroot\\img\\alex-shaw-7TntdeL9FZ0-unsplash.jpg")), ImageType = ".png" },
+                new Contact() {AppUserId = User.Id,FirstName = "Joan", LastName = "Smith", Address1 = "1234 Apple Street", BirthDate = new DateTime(1994, 08, 16), City = "Paris", State = Enums.States.TX, Email = "test@test.com", ZipCode = 21000, PhoneNumber = "0000000000", ImageData = File.ReadAllBytes(Path.Combine(currentDirectory, "wwwroot//img//alex-shaw-7TntdeL9FZ0-unsplash.jpg")), ImageType = ".png" },
 
-                new Contact() {AppUserId = User.Id, FirstName = "Erin", LastName = "Doe", Address1 = "1 Katz Place", BirthDate = new DateTime(1976, 01, 02), City = "Charleston", State = Enums.States.WV, Email = "test@test.com", ZipCode = 18701, PhoneNumber = "0000000000", ImageData = File.ReadAllBytes(Path.Combine(currentDirectory, "wwwroot\\img\\megan-ruth-nBbB795RRV4-unsplash.jpg")), ImageType = ".png" },
+                new Contact() {AppUserId = User.Id, FirstName = "Erin", LastName = "Doe", Address1 = "1 Katz Place", BirthDate = new DateTime(1976, 01, 02), City = "Charleston", State = Enums.States.WV, Email = "test@test.com", ZipCode = 18701, PhoneNumber = "0000000000", ImageData = File.ReadAllBytes(Path.Combine(currentDirectory, "wwwroot//img//megan-ruth-nBbB795RRV4-unsplash.jpg")), ImageType = ".png" },
 
-                new Contact() {AppUserId = User.Id, FirstName = "Ty", LastName = "Long", Address1 = "11 Dell Place", BirthDate = new DateTime(1979, 05, 12), City = "Roanoke", State = Enums.States.VA, Email = "test@test.com", ZipCode = 18701, PhoneNumber = "0000000000", ImageData = File.ReadAllBytes(Path.Combine(currentDirectory, "wwwroot\\img\\marya_volk-SbYRkfmgwUU-unsplash.jpg")), ImageType = ".png" }
+                new Contact() {AppUserId = User.Id, FirstName = "Ty", LastName = "Long", Address1 = "11 Dell Place", BirthDate = new DateTime(1979, 05, 12), City = "Roanoke", State = Enums.States.VA, Email = "test@test.com", ZipCode = 18701, PhoneNumber = "0000000000", ImageData = File.ReadAllBytes(Path.Combine(currentDirectory, "wwwroot//img//marya_volk-SbYRkfmgwUU-unsplash.jpg")), ImageType = ".png" }
             };
             }
 
